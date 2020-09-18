ad64b4cb58fce56b6abb13f460ff3f4c9992aa5d
Ashuto7h <ashusahu198@gmail.com>
2020-09-18 23:30:30 +0530

    added styles to navbar 2

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static('src/js'));
 app.use(express.static('src'));
 app.use(express.static('src/html'));
 app.use('/assets',express.static('assets'))
+
 app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
@@ -20,3 +21,4 @@ app.get('/u', function (req, res) {
 app.get('/u/nav.html', function (req, res) {
     res.sendFile('src/html/nav.html', { root: __dirname });
 });
+
