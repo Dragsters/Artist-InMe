e7ba48a4a9cbeab0cf69db5849836b0683c4580c
Ashuto7h <ashusahu198@gmail.com>
2020-10-07 10:47:21 +0530

    signin request created

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 const server = app.listen(PORT, function () {
-    console.log('Node server listening at ', PORT);
+    console.log('Node server listening at http://localhost:' + PORT);
 });
 
 app.use(express.static('src/js'));
 app.use(express.static('src'));
 app.use(express.static('src/html'));
 app.use('/assets', express.static('assets'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
@@ -35,4 +37,9 @@ app.get('/signup', function (req, res) {
 
 app.get('/signmode', function (req, res) {
     res.send(JSON.stringify({ signin: signin }))
-})
+});
+
+app.post('/auth', function (req, res) {
+    console.log(req.body);
+    res.send(JSON.stringify({ message: "auth req reached." }));
+});
--- src/js/signinup.js
+++ src/js/signinup.js
@@ -1,19 +1,32 @@
 
-// var x = document.getElementById("signin");
-// var y = document.getElementById("signup");
-var z = document.getElementsByClassName("btn-layer");
+var sign_in_up_form = document.getElementsByClassName("sign-in-up-form");
+var signin_form = document.getElementsByClassName("signin-form");
+var signup_form = document.getElementsByClassName("signup-form");
+var btn_layer = document.getElementsByClassName("btn-layer");
+var toggle_btn = document.getElementsByClassName("toggle-btn")
+var btn_signin = toggle_btn[0];
+var btn_signup = toggle_btn[1];
 function signup() {
-    // x.style.left = "-400px";
-    // y.style.left = "50px";
-    z[0].style.left = "50%";
+    sign_in_up_form[0].style.transform = "translateX(-50%)";
+    signup_form[0].style.opacity = 1;
+    signin_form[0].style.opacity = 0;
+    btn_layer[0].style.left = "50%";
+    btn_signup.style.color = "#0D131F"
+    btn_signin.style.color = "white"
 }
 
 function signin() {
-    // x.style.left = "50px";
-    // y.style.left = "450px";
-    z[0].style.left = "0%";
+    sign_in_up_form[0].style.transform = "translateX(0%)";
+    signup_form[0].style.opacity = 0;
+    signin_form[0].style.opacity = 1;
+    btn_layer[0].style.left = "0%";
+    btn_signin.style.color = "#0D131F"
+    btn_signup.style.color = "white"
 }
 
+function forgot() {
+    alert('processing...');
+}
 var signInMode = false;
 fetch('http://localhost:3000/signmode')
     .then((response) => {
@@ -27,3 +40,19 @@ fetch('http://localhost:3000/signmode')
             signup();
     });
 
+$("#signin-form").submit(function (e) {
+    e.preventDefault();
+    var inputvals = $("#signin-form input").serializeArray();
+    var data = { user: inputvals[0].value, pass: inputvals[1].value }
+    fetch('http://localhost:3000/auth', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    });
+});
